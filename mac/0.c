54a5264ece1264f8a8c6ce58172c3f2d6d7b876b
Eduardo Júnior <efduardo@outlook.com>
2022-04-10 08:58:10 -0300

    Create 0.c

---
+++ mac/0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int inteiro;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &inteiro);
+
+    printf("\nO numero digitado foi %d", inteiro);
+
+    return 0;
+}
