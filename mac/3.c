c55d5d41a4fb6f46b597db736613498a7be2e411
Eduardo Júnior <efduardo@outlook.com>
2022-04-12 08:40:40 -0300

    Create 3.c

---
+++ mac/3.c
@@ -0,0 +1,21 @@
+//Este algoritmo vai ler dois números inteiros e imprimir o quadrado da diferença entre eles.
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int entrada1, entrada2, quadradodadiferenca;
+
+	printf("Digite um numero inteiro: ");
+	scanf("%d", &entrada1);
+
+	printf("Digite outro numero inteiro: ");
+	scanf("%d", &entrada2);
+
+	quadradodadiferenca = pow((entrada1 - entrada2), 2);
+
+	printf("O quadrado da diferenca dos numeros digitados e %d", quadradodadiferenca);
+
+	return 0;
+}
