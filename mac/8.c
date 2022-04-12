7997dda8e3509e8d71700cbeed76892d4b3e3591
Eduardo Júnior <efduardo@outlook.com>
2022-04-12 10:17:48 -0300

    Create 8.c

---
+++ mac/8.c
@@ -0,0 +1,19 @@
+//Este é um algoritmo que recebe dois números inteiros como entradas e imprime 1 se ocorrer entrada1 < entrada2, e 0 se ocorrer entrada1 >= entrada2.
+#include <stdio.h>
+
+int main()
+{
+    int variavel1, variavel2, auxiliar;
+    
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &variavel1);
+    
+    printf("Digite outro numero inteiro: ");
+    scanf("%d", &variavel2);
+    
+    printf("%d ", variavel2);
+    printf("%d", variavel1);
+    scanf("%d", auxiliar);
+    
+    return 0;
+}
