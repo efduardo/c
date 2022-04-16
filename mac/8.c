722eec22bcf0d9187d8f574dda49ecb9b82fcd4a
Eduardo Júnior <efduardo@outlook.com>
2022-04-16 20:34:28 -0300

    Update 8.c

--- mac/8.c
+++ mac/8.c
@@ -1,4 +1,5 @@
 //Este algoritmo solicita que o usuário digite 2 valores inteiros, armazenando o primeiro em uma variável e o segundo em outra variável; feito isto, utiliza uma nova variável (auxiliar) para trocar o conteúdo armazenado na primeira variável com o da segunda variável; finalmente imprime o valor da segunda e o da primeira, nesta ordem!
+
 #include <stdio.h>
 
 int main()
