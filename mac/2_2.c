d6f7deadf475a147ede617f0347200a6d879d203
Eduardo Júnior <efduardo@hotmail.com>
2022-04-21 20:46:30 -0300

    Update 2_2.c

--- mac/2_2.c
+++ mac/2_2.c
@@ -1,13 +1,12 @@
 //Este programa vai ler dois números inteiros e imprimir a sua soma.
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void)
+int main()
 {
     int entrada1, entrada2, soma;
 
-    printf("Digite um número inteiro: ");
+    printf("Digite um numero inteiro: ");
     scanf("%d", &entrada1);
 
     printf("Digite outro numero inteiro: ");
@@ -15,5 +14,5 @@ int main(void)
 
     soma = entrada1 + entrada2;
 
-    printf("A soma dos números digitados é %d", soma);
+    printf("A soma dos numeros digitados e %d", soma);
 }
