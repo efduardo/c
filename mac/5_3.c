d171054da9dc3f6ca50ece4da9d26571180424cc
Eduardo Júnior <efduardo@hotmail.com>
2022-04-21 19:27:40 -0300

    Create 5_3.c

---
+++ mac/5_3.c
@@ -0,0 +1,31 @@
+//Este programa verifica se 3 números podem ou não representar os ângulos de um triângulo. O algoritmo solicita que o usuário digite 3 números naturais (representando ângulos em graus) e imprime "Sim!" e os 3 números na sequência digitada, se somarem 180; e "Não!" e a soma dos 3 números em caso contrário.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int anguloa, angulob, anguloc, soma;
+
+    printf("Digite o valor do primeiro angulo: ");
+    scanf("%d", &anguloa);
+
+    printf("Digite o valor do segundo angulo: ");
+    scanf("%d", &angulob);
+
+    printf("Digite o valor do terceiro angulo: ");
+    scanf("%d", &anguloc);
+
+    while((anguloa > 0) && (angulob > 0) && (anguloc > 0))
+    {
+        soma = anguloa + angulob + anguloc;
+
+        if (soma == 180)
+            printf("Sim! %d %d %d", anguloa, angulob, anguloc);
+        else
+            printf("Nao! %d", soma);
+        break;
+    }
+
+    return 0;
+}
