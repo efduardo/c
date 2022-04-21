37835be02bb0730e4feeddfe558e4217aa0f8d92
Eduardo Júnior <efduardo@hotmail.com>
2022-04-21 19:14:13 -0300

    Create 5_2.c

---
+++ mac/5_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int entrada1, entrada2, entrada3;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &entrada1);
+
+    printf("Digite outro numero inteiro: ");
+    scanf("%d", &entrada2);
+
+    printf("Digite mais um numero inteiro: ");
+    scanf("%d", &entrada3);
+
+    if ((entrada1 >= entrada2) && (entrada2 >= entrada3))
+        printf("%d %d %d", entrada1, entrada2, entrada3);
+    if ((entrada1 >= entrada3) && (entrada3 >= entrada2))
+        printf("%d %d %d", entrada1, entrada3, entrada2);
+    if ((entrada2 >= entrada1) && (entrada1 >= entrada3))
+        printf("%d %d %d", entrada2, entrada1, entrada3);
+    if ((entrada2 >= entrada3) && (entrada3 >= entrada1))
+        printf("%d %d %d", entrada2, entrada3, entrada1);
+    if ((entrada3 >= entrada1) && (entrada1 >= entrada2))
+        printf("%d %d %d", entrada3, entrada1, entrada2);
+    else
+        printf("%d %d %d", entrada3, entrada2, entrada1);
+
+    return 0;
+}
